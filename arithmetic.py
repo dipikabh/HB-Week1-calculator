12dd6eb054cfce1e1338757d838a5c59f3bc89d3
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 12:28:01 -0800

    Implemented fixed addition logic

---
+++ arithmetic.py
@@ -0,0 +1,35 @@
+"""Math functions for calculator."""
+
+
+def add(num1, num2):
+    """Return the sum of the two inputs."""
+
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    """Return the second number subtracted from the first."""
+
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+
+
+def divide(num1, num2):
+    """Divide the first input by the second and return the result."""
+
+
+def square(num1):
+    """Return the square of the input."""
+
+
+def cube(num1):
+    """Return the cube of the input."""
+
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
