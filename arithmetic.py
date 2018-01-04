807834dfe674c97175bb7fbaa1ea3a6430a16ab5
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 12:48:29 -0800

    Fully functional prefix calculator, yay!

--- arithmetic.py
+++ arithmetic.py
@@ -43,6 +43,10 @@ def cube(num1):
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    return num1 ** num2
+
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+
+    return num1 % num2
