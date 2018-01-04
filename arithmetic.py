a70495d0f230a5ea202f98ecc2079ac7f7fd11ba
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 12:43:22 -0800

    Added square and cube features

--- arithmetic.py
+++ arithmetic.py
@@ -31,16 +31,17 @@ def divide(num1, num2):
 def square(num1):
     """Return the square of the input."""
 
+    return num1 ** 2
 
 
 def cube(num1):
     """Return the cube of the input."""
 
+    return num1 ** 3
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
 
 
 def mod(num1, num2):
